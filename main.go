f5e9c4f9e7d16227f30ff0ea1e81b61ffa018556
0187773933 <collincerbus@student.olympic.edu>
2023-09-13 10:31:49 -0400

    5

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// "fmt"
 	types "github.com/0187773933/LGTVController/v1/types"
 	utils "github.com/0187773933/LGTVController/v1/utils"
 	lg_tv "github.com/0187773933/LGTVController/v1/controller"
@@ -12,10 +11,8 @@ func main() {
 	tv := lg_tv.New( &config )
 
 	tv.API( "power_on" )
-
 	tv.API( "set_volume", types.Payload{
 		"volume": 15 ,
 	})
-
 	tv.API( "set_mute" )
 }
